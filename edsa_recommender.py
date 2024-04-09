58c3ac332638d0beb910bbd3381b1a65fde36e44
KamogeloMorole <149101907+KamogeloMorole@users.noreply.github.com>
2024-04-10 00:38:25 +0200

    new pages

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -112,7 +112,7 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-add_bg_from_local('resources/em.avif')  
+add_bg_from_local('resources/imgs/back.jpg')  
 
 def main():
     selected = option_menu(
